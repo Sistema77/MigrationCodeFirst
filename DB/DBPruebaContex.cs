580815d96907bbb319bc933c287b3a60d5f0139b
Jose Manuel <josemanuelmorenoguerra96@gmail.com>
2023-10-24 20:31:23 +0200

    first commit

---
+++ DB/DBPruebaContex.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DB
+{
+    public class DBPruebaContex : DbContext
+    {
+        public DBPruebaContex(DbContextOptions<DBPruebaContex> options) 
+            : base(options) 
+        {
+        
+        }
+
+        public DbSet<Libros> libro { get; set; }
+    }
+}
---
+++ DB/Libros.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    public class Libros
+    {
+        [Key] // Para que el idLibro sea el Primary Key
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Primary Key AutoCompetable
+        public int idLibro { get; set; }
+        public string name { get; set; }
+    }
+}
